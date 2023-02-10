6b281f673d837c7a9b2e19cec2eafd51e5fbd9f0
abdulwahabshukri <abdulwahab.shukri@student.techstarter.de>
2023-02-10 17:07:13 +0100

    Update

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
 }
 
 provider "azurerm" {
-  
+  /*
     subscription_id = "ea6e6692-4d05-4c5b-9909-51c7dc5f5c2b"
     client_id       = "d8a08696-446d-4926-a979-a2a871833c0a"
     client_secret   = "UeF8Q~YG_ta2S4xEZmlzG5trwvVh2SgDxG9TDc3R"
     tenant_id       = "4dfdfd67-3a37-4e2e-b9f0-434c7061ba33"
-    
+   */ 
     features {
       
     }
